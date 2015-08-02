4c743a4d7a6c70d42d989b8f0d008cf6ad998ebc
alumno <alumno@Lab1504-05>
2015-08-02 11:07:37 -0500

    ejercicio 1

--- src/main/java/com/tecsup/primefaces/bean/MapaBean.java
+++ src/main/java/com/tecsup/primefaces/bean/MapaBean.java
@@ -5,22 +5,25 @@
 package com.tecsup.primefaces.bean;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
+import org.primefaces.model.map.Circle;
 import org.primefaces.model.map.DefaultMapModel;
 import org.primefaces.model.map.LatLng;
 import org.primefaces.model.map.MapModel;
 import org.primefaces.model.map.Marker;
 
 
-/**
- *
- * @author alumno
- */
+@ManagedBean
 public class MapaBean {
 
     private MapModel modelo = new DefaultMapModel();
 
 	public MapaBean() {
+            
+                Circle circle = new Circle(new LatLng(-12.043729, -76.953144), 500d);
+                circle.setFillColor("red");
+                circle.setFillOpacity(0.2d);
+                modelo.addOverlay(circle);
+                
 		modelo.addOverlay(new Marker(new LatLng(-12.043729, -76.953144), "Tecsup"));
 		modelo.addOverlay(new Marker(new LatLng(-12.0421367, -76.9501667), "BCP - Mercado Productores"));
 	}
